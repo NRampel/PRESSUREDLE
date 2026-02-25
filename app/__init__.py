251745e07adf18588d8907f317cab8272274b96b
Nicolas <nrampelotto414@gmail.com>
2026-02-25 12:23:07 -0500

    Cached Data
    Cached the data, so now the game runs faster

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask 
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy 
+from flask_session import Session
 
 db = SQLAlchemy()
+sess = Session()
 
 def create_app(): 
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
     db.init_app(app)
+    sess.init_app(app)
     from app.routes.main import main_bp
     from app.routes.game import game_bp
     from app.routes.auth import auth_bp    
--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from dotenv import load_dotenv
 
 
@@ -18,3 +18,6 @@ class Config:
     }
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_TYPE = 'filesystem'
+    SESSION_PERMANENT = False 
+    SESSION_USE_SIGNER = True
